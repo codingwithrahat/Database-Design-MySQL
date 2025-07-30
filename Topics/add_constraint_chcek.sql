a41544ec8e2c2fbf24b147fbc3371315003fc0dc
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-07-30 20:02:27 +0600

    Update add_constraint_chcek.sql

--- Topics/add_constraint_chcek.sql
+++ Topics/add_constraint_chcek.sql
@@ -2,5 +2,5 @@ alter table students
 add constraint chk_age CHECK (Age >= 18);
 
 -- adds a check constraint name "chk_age" to the students table age column
--- i set the name of the contraint to "chk_age", but it can be anything i want
+-- i set the name of the constraint to "chk_age", but it can be anything i want
 -- if a person inputs an age less than 18, it will give an error
