87ac34a669ddc408e15ca5db3c3bef2ea5b09458
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-07-30 19:43:12 +0600

    Create modify_table.sql

---
+++ Topics/modify_table.sql
@@ -0,0 +1,8 @@
+alter table students
+modify age int(20);
+
+-- diff between int and numeric
+-- numeric means decimal
+-- decimal d(5,1) 
+-- total 5 digit number with 1 digit after decimal point
+-- if i don't mention the size, it will take size 11 (by default)
