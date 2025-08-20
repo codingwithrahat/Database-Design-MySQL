ad2fe561906cdbda63eed190a75aaec15ea97981
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-08-20 23:35:06 +0600

    Create order_by_2.sql

---
+++ Topics/order_by_2.sql
@@ -0,0 +1,5 @@
+select * from student 
+order by age desc;
+
+-- ORDER BY sorts the result set based on a column (age in this case).
+-- Using DESC sorts the result set in descending order (from largest to smallest).
