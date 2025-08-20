18039f053d526869affd4fc37ce1abd5ea9d8810
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-08-20 23:56:40 +0600

    Create string_op_3.sql

---
+++ Topics/string_op_3.sql
@@ -0,0 +1,4 @@
+select * from student where name like '_____';
+
+-- '_____' (5 underscores) means the name must be exactly 5 characters long (Rahat, Rakib)
+-- Each underscore (_) represents exactly one character 
