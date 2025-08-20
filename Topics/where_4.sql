8d37d6486c9fe3600d7b6c0a573da6b0bd7458fb
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-08-20 23:16:13 +0600

    Create where_4.sql

---
+++ Topics/where_4.sql
@@ -0,0 +1,5 @@
+select name from student where age between 16 and 20;
+
+-- BETWEEN checks if a value is within a range (inclusive)
+-- This is the same as: age >= 16 AND age <= 20
+-- Here, it returns the names of students whose age is between 16 and 20
