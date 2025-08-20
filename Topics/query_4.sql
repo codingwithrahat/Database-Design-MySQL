6f01da0dc91369b2300f4f248ea1ad4061d2c6ff
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-08-20 20:54:16 +0600

    Create query_4.sql

---
+++ Topics/query_4.sql
@@ -0,0 +1,5 @@
+select name from student where age + 10 >= 26;
+
+-- WHERE can also perform operations
+-- Here, it checks if the student's age plus 10 is greater than or equal to 26
+-- If the condition is true, the student's name will be returned
