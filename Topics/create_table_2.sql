7ca3d9d5ee69c53363154eb301fce849a1316604
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-08-20 20:20:22 +0600

    Create create_table_2.sql

---
+++ Topics/create_table_2.sql
@@ -0,0 +1,7 @@
+create table student2(
+    id int,
+    name varchar(50) unique, -- all stu name should be unqiue
+    age int not null,
+    primary key (id) -- primery key constraints added in id , another way to add constraints
+    );
+    
