92e5cb18014065788ce6b72ec7d94d15a2ee590f
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-08-20 23:54:39 +0600

    Create string_op_2.sql

---
+++ Topics/string_op_2.sql
@@ -0,0 +1,4 @@
+select * from student where name like 'Ra%';
+
+-- 'Ra%' means the name must start with 'Ra' and can have any characters after it
+-- This will return all rows where the name begins with 'Ra' (Rahat', Rakib)
