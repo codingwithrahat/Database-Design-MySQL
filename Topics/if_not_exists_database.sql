fd0f1a810c75aa1c04d433704c94ce68d65ea80f
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-08-20 19:52:55 +0600

    Create if_not_exists_database.sql

---
+++ Topics/if_not_exists_database.sql
@@ -0,0 +1,5 @@
+ create databse if not exists temp;
+
+-- If the 'temp' database does not exist, it will be created
+-- If it already exists, the statement is ignored
+-- Without 'IF NOT EXISTS', an error occurs if 'temp' already exists
