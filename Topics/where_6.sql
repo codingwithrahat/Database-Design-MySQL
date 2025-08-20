5ed360abb16905440d013134935114efabe3e3e8
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-08-20 23:25:40 +0600

    Create where_6.sql

---
+++ Topics/where_6.sql
@@ -0,0 +1,4 @@
+select name, age from student where age + 4 >= 20;
+
+-- This checks if the student's age plus 4 is greater than or equal to 20.
+-- If the condition is true, the student's name and age will be returned.
