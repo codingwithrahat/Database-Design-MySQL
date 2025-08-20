3eb18783bc8f37d49ecc10d35f54a9d8e7096555
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-08-20 23:33:57 +0600

    Create order_by_1.sql

---
+++ Topics/order_by_1.sql
@@ -0,0 +1,6 @@
+select * from student 
+order by age;
+
+-- ORDER BY sorts the result set based on a column (age in this case).
+-- By default, it sorts in ascending order (from smallest to largest).
+-- You can also explicitly use ASC (ORDER BY age ASC) for ascending order.
