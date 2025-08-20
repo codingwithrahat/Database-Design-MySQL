e61f424ecdeb2729fa389343ac404f0ee9b86417
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-08-20 20:42:54 +0600

    Update foreign_key.sql

--- Topics/foreign_key.sql
+++ Topics/foreign_key.sql
@@ -9,3 +9,10 @@ create table Projects(
     
     
     );
+
+    -- Projects table Manager_ID is foreign key
+    -- Employee_ID must be primary key
+    -- Manager_ID references employees table Employee_ID
+    -- it means if someone input a Manager_ID that don't match with Any of Employee_ID it will return error
+    -- Manager_ID can be duplicate
+    -- it also can be null vale
