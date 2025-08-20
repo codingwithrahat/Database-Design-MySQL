32fa3ea973742c072a6795fcdf37499bef45edfa
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-08-20 20:07:15 +0600

    Create insert_values_2.sql

---
+++ Topics/insert_values_2.sql
@@ -0,0 +1,4 @@
+insert into student (age , id, name) values (16, 3, 'sarawer');
+
+-- The student table schema is id, name, age
+-- But here I am inserting values in a different order: age first, then id, then name
