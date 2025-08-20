9c430f69c5e9a15f957c79099c78e2b08c36928c
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-08-20 19:54:09 +0600

    Create if_exists_database.sql

---
+++ Topics/if_exists_database.sql
@@ -0,0 +1,5 @@
+drop database if exists temp;
+
+-- Deletes the 'temp' database if it exists
+-- If the database does not exist, the statement does nothing
+-- Without 'IF EXISTS', an error occurs if 'temp' does not exist
