f8bb4f729123b2b4c462fe1b763dddd8ee9667fa
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-08-20 23:53:09 +0600

    Create string_op_1.sql

---
+++ Topics/string_op_1.sql
@@ -0,0 +1,4 @@
+select * from student where name like '%ha%';
+
+-- '%ha%' means the name can have any characters before and after 'ha'
+-- This will return all rows where the name contains 'ha' anywhere (rahat)
