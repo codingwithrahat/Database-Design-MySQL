3c5201296ea880efa04f2c37dd35875da6f4e988
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-08-20 23:22:22 +0600

    Create where_2.sql

---
+++ Topics/where_2.sql
@@ -0,0 +1,5 @@
+select name from student where id = 1 or id = 2;
+
+-- WHERE can also use logical operators like AND / OR.
+-- Here, it checks if the student's id is either 1 or 2.
+-- If the condition is true, the student's name will be returned.
