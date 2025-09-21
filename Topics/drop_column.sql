986d2667e7453165dcba64166eb0293afba6488a
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-09-21 23:25:14 +0600

    Update drop_column.sql

--- Topics/drop_column.sql
+++ Topics/drop_column.sql
@@ -1,5 +1,7 @@
 alter table students
 drop column email;
 
+-- Alter the structure of the "students" table
+-- here using alter is mandotary
 -- if i don't use column ,it will still work,
 -- but including column is better
