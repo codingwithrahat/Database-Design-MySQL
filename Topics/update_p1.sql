f82a2227ac7ed3ae8a146e2ac469851738cd9353
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-09-21 23:14:32 +0600

    Create update_p1.sql

---
+++ Topics/update_p1.sql
@@ -0,0 +1,7 @@
+UPDATE students 
+SET grade = 'O'
+WHERE grade = 'A+';
+
+-- Update the "students" table
+-- Change the value of column "grade" to 'O'
+-- Only for those rows where the current grade is 'A+'
