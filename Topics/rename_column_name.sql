470451147c5e5c4e8e895faafbdd2501aaa59b75
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-09-22 00:33:15 +0600

    Create rename_column_name.sql

---
+++ Topics/rename_column_name.sql
@@ -0,0 +1,8 @@
+alter table stu
+change column age stu_age1 int;
+
+-- Alter the "stu" table
+-- Rename column "age" to "stu_age"
+-- IMPORTANT: In CHANGE COLUMN, you must re-define the datatype
+-- (and any constraints like NOT NULL, UNIQUE, etc.)
+
